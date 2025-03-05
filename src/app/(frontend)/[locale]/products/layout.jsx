4ce2c7e45cc280d39d318c0b37e8564c1bdefab4
Muhamedsafwat <musafwat666@gmail.com>
2025-03-06 01:27:06 +0200

    Refactor project structure and update routing for products

--- src/app/(frontend)/[locale]/products/layout.jsx
+++ src/app/(frontend)/[locale]/products/layout.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 import { IoHome } from "react-icons/io5";
 import { useParams } from "next/navigation";
+import Link from "next/link";
 
 const layout = ({ children }) => {
   const t = useTranslations("SingleProduct");
@@ -31,13 +32,18 @@ const layout = ({ children }) => {
   return (
     <div className="container">
       <div className="mx-auto flex w-full justify-center mt-20 items-center gap-2 cursor-pointer text-white text-xl mb-5">
-        <div className="hover:text-[#d4af37] flex items-center gap-1 mb-[1px]">
+        <Link
+          href={`/${locale}`}
+          className="hover:text-[#d4af37] flex items-center gap-1 mb-[1px]"
+        >
           <IoHome className="text-xl mr-2 mt-[1px]" />
           <span>{t("Home")}</span>
-        </div>
-        <span className="hover:text-[#d4af37]">{">"}</span>
-        <span className="hover:text-[#d4af37]">{t("Product")}</span>
-        <span className="hover:text-[#d4af37]">{">"}</span>
+        </Link>
+        <span>{">"}</span>
+        <Link href={`/${locale}#products`} className="hover:text-[#d4af37]">
+          {t("Product")}
+        </Link>
+        <span>{">"}</span>
         <span className="font-semibold hover:text-[#d4af37]">{category}</span>
       </div>
       {children}
